b1941f09dda9852e85613f8ab3bbebaff5459f15
Václav Černý <vatoz@seznam.cz>
2020-11-01 20:33:28 +0100

    f2

--- db.php
+++ db.php
@@ -1,5 +1,8 @@
 <?php
-
+/*
+*Varací všechny uživatele
+*@todo vrátit jen aktivní, a vymyslet pořadí
+*/
 function getActiveUsers(){
   global $pdo;
   $result = $pdo->query('SELECT * FROM users');
@@ -9,10 +12,34 @@ function getActiveUsers(){
   }
   return $ret;
 }
+/*
+/*Vrátí uživatele
+*/
+function getUser($User,$Password){
+  //$k= getActiveUsers();  foreach($k as $u){    return $u;  }
+  $sql = 'SELECT * FROM users WHERE password=:p';
 
-function getTopics(){
   global $pdo;
-  $result = $pdo->query("SELECT id,name FROM topics where since_when < datetime('now') ");
+  $stmt = $pdo->prepare($sql);
+//  $stmt->bindValue(':u', $User);
+  //$stmt->bindValue();
+  $result=$stmt->execute([':p'=>$Password]);
+  //error_log("qqq".var_export($result,true));
+  if($result==false) return false;
+
+  while($row=  $stmt->fetch(\PDO::FETCH_ASSOC)){
+    return $row;
+  }
+
+}
+
+/*
+/*Vrátí témata.
+*@param Active Ve výchozím stavu jen ty už aktivní
+*/
+function getTopics($Active=true){
+  global $pdo;
+  $result = $pdo->query("SELECT id,name FROM topics " .($Active?"where since_when < datetime('now') ":""));
   $ret=array();
   foreach($result as $row){
     $ret[$row['id']]= $row['name'];
@@ -20,7 +47,17 @@ function getTopics(){
   return $ret;
 }
 
+function f2($t){
+  $k=''.$t;
+  if(strlen($k)){
+    $k='0'.$k;
+  }
+  return $k;
+}
 
+/*
+* Vrátí aktuality. Naplánuji na ně editor, moc jich asi nebude.
+*/
 function  getEditorials(){
   global $pdo;
   $result = $pdo->query('SELECT * FROM editorials');
@@ -31,8 +68,10 @@ function  getEditorials(){
   return $ret;
 
 }
-
-function getPosts(){
+/*
+*NAčte z db všechny příspěvky, nebo příspěvky jednoho uživatele
+*/
+function getPosts($User=0){
   global $pdo;
-  return $pdo->query('SELECT * FROM posts ORDER BY topic ASC');
+  return $pdo->query('SELECT * FROM posts '.($User>0 ?" WHERE author= ".$User:"").' ORDER BY topic ASC');
 }
